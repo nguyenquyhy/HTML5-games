6b44aef0a3c0cb1fb4bc1764e9f7027ee05c9611
nguyenquyhy@live.com.sg <nguyenquyhy@live.com.sg>
2015-09-10 03:52:28 +0800

    Touch & mouse event.

--- src/Games/ts/pong/game.ts
+++ src/Games/ts/pong/game.ts
@@ -4,6 +4,7 @@
     ball: Entity;
     players: ControlBar[];
     keys = {};
+    mouse: Vector2 = null;
 
     constructor(context: CanvasRenderingContext2D) {
         this.context = context;
@@ -39,11 +40,35 @@
         delete this.keys[e.keyCode];
     }
 
+    doMouseDown(e) {
+        // TODO: support multi-touch
+        if (e.buttons > 0) {
+            this.mouse = new Vector2(e.offsetX, e.offsetY);
+        }
+    }
+
+    doMouseMove(e) {
+        if (e.buttons > 0) {
+            this.mouse = new Vector2(e.offsetX, e.offsetY);
+        }
+    }
+
+    doMouseUp(e) {
+        this.mouse = null;
+    }
+
     update(elapsed: number) {
         if (this.keys[38])
             this.players[0].up();
         else if (this.keys[40])
             this.players[0].down();
+
+        if (this.mouse !== null) {
+            if (this.mouse.y > this.context.canvas.height / 2)
+                this.players[0].down();
+            else
+                this.players[0].up();
+        }
 
         if (this.entities) {
             for (var i = 0; i < this.entities.length; i++) {
@@ -81,8 +106,9 @@ function tick() {
 }
 
 var canvas = <HTMLCanvasElement>document.getElementById('mainCanvas');
+var context: CanvasRenderingContext2D;
 if (canvas.getContext) {
-    var context = canvas.getContext('2d');
+    context = canvas.getContext('2d');
     game = new Game(context);
 
     window.addEventListener("keydown", function (e) {
@@ -92,6 +118,33 @@ if (canvas.getContext) {
     window.addEventListener("keyup", function (e) {
         game.doKeyUp(e);
     }, true);
+
+    if (window['PointerEvent']) {
+        canvas.addEventListener("pointermove", function (event) {
+            game.doMouseMove(event);
+        }, false);
+        canvas.addEventListener("pointerdown", function (event) {
+            game.doMouseDown(event);
+        }, false);
+        canvas.addEventListener("pointerup", function (event) {
+            game.doMouseUp(event);
+        }, false);
+
+        //if (window.navigator.maxTouchPoints > 1)
+    	   /* User agent and hardware support multi-touch */
+    }
+    else {
+        //Provide fallback for user agents that do not support Pointer Events
+        canvas.addEventListener("mousemove", function (event) {
+            game.doMouseMove(event);
+        }, false);
+        canvas.addEventListener("mousedown", function (event) {
+            game.doMouseDown(event);
+        }, false);
+        canvas.addEventListener("mouseup", function (event) {
+            game.doMouseUp(event);
+        }, false);
+    }
 
     window.requestAnimationFrame = window.requestAnimationFrame || window['webkitRequestAnimationFrame'] || window['msRequestAnimationFrame'] || window['mozRequestAnimationFrame'];
 
--- src/Games/wwwroot/js/pong/game.js
+++ src/Games/wwwroot/js/pong/game.js
@@ -1,6 +1,7 @@
 var Game = (function () {
     function Game(context) {
         this.keys = {};
+        this.mouse = null;
         this.context = context;
         var initialSpeed = 200;
         this.ball = new Circle(this.context, 0.5 * context.canvas.width, 0.5 * context.canvas.height, initialSpeed * Math.cos(Math.random() * 2 * Math.PI), initialSpeed * Math.sin(Math.random() * 2 * Math.PI), 12);
@@ -20,11 +21,30 @@ var Game = (function () {
     Game.prototype.doKeyUp = function (e) {
         delete this.keys[e.keyCode];
     };
+    Game.prototype.doMouseDown = function (e) {
+        if (e.buttons > 0) {
+            this.mouse = new Vector2(e.offsetX, e.offsetY);
+        }
+    };
+    Game.prototype.doMouseMove = function (e) {
+        if (e.buttons > 0) {
+            this.mouse = new Vector2(e.offsetX, e.offsetY);
+        }
+    };
+    Game.prototype.doMouseUp = function (e) {
+        this.mouse = null;
+    };
     Game.prototype.update = function (elapsed) {
         if (this.keys[38])
             this.players[0].up();
         else if (this.keys[40])
             this.players[0].down();
+        if (this.mouse !== null) {
+            if (this.mouse.y > this.context.canvas.height / 2)
+                this.players[0].down();
+            else
+                this.players[0].up();
+        }
         if (this.entities) {
             for (var i = 0; i < this.entities.length; i++) {
                 this.entities[i].update(elapsed);
@@ -57,8 +77,9 @@ function tick() {
     window.requestAnimationFrame(tick);
 }
 var canvas = document.getElementById('mainCanvas');
+var context;
 if (canvas.getContext) {
-    var context = canvas.getContext('2d');
+    context = canvas.getContext('2d');
     game = new Game(context);
     window.addEventListener("keydown", function (e) {
         game.doKeyDown(e);
@@ -66,6 +87,28 @@ if (canvas.getContext) {
     window.addEventListener("keyup", function (e) {
         game.doKeyUp(e);
     }, true);
+    if (window['PointerEvent']) {
+        canvas.addEventListener("pointermove", function (event) {
+            game.doMouseMove(event);
+        }, false);
+        canvas.addEventListener("pointerdown", function (event) {
+            game.doMouseDown(event);
+        }, false);
+        canvas.addEventListener("pointerup", function (event) {
+            game.doMouseUp(event);
+        }, false);
+    }
+    else {
+        canvas.addEventListener("mousemove", function (event) {
+            game.doMouseMove(event);
+        }, false);
+        canvas.addEventListener("mousedown", function (event) {
+            game.doMouseDown(event);
+        }, false);
+        canvas.addEventListener("mouseup", function (event) {
+            game.doMouseUp(event);
+        }, false);
+    }
     window.requestAnimationFrame = window.requestAnimationFrame || window['webkitRequestAnimationFrame'] || window['msRequestAnimationFrame'] || window['mozRequestAnimationFrame'];
     tick();
 }
